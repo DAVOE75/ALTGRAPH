9765b59b48bf9b3a50bd4d6d94aa92eadc4e8186
DAVOE75 <DAVOE75@users.noreply.github.com>
2026-09-25 18:59:09 +0200

    Increment version code and KarooExtension version to force cache invalidation

--- app/src/main/java/com/example/altgraph/AltimetriaExtensionService.kt
+++ app/src/main/java/com/example/altgraph/AltimetriaExtensionService.kt
@@ -3,7 +3,7 @@ package com.example.altgraph
 import io.hammerhead.karooext.extension.DataTypeImpl
 import io.hammerhead.karooext.extension.KarooExtension
 
-class AltimetriaExtensionService : KarooExtension("altgraph", "0.4.62") {
+class AltimetriaExtensionService : KarooExtension("altgraph", "0.4.63") {
 
     override val types: List<DataTypeImpl> by lazy {
         listOf(
